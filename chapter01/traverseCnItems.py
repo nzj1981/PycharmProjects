38c96e842aefd160eeeaaf382ee6018c477c36f2
nzj1981 <18322313385@163.com>
2017-11-02 21:04:19 +0800

    traverseCnItems.py update 4time

--- chapter01/traverseCnItems.py
+++ chapter01/traverseCnItems.py
@@ -10,16 +10,18 @@ allGuests = {'Alice': {'apples': 3, 'pretzels': 4}, 'Bob': {'ham snadwiches': 3,
 def totalBrought(guests, item):
     # initiate a counter
     numBrought = 0
-    # tranverse vlaues of all guests
+    # traverse vlaues of all guests
     for v in guests.values():
         # accumulate the item
         numBrought += v.get(item, 0)
     return numBrought
-
+# Initialize the collection
+fruits = set()
+# Get a collection of fruits
+for v in allGuests.values():
+    fruits |= set(v)
+# print(fruits)
 # print number of items (call function)
 print("Number of things being brouht: \n")
-print("-Apple            {:>4}".format(totalBrought(allGuests,'apples',)))
-print("-pretzels         {:>4}".format(totalBrought(allGuests,'pretzels',)))
-print("-ham snadwiches   {:>4}".format(totalBrought(allGuests,'ham snadwiches',)))
-print("-cups             {:>4}".format(totalBrought(allGuests,'cups',)))
-print("-apple pies       {:>4}".format(totalBrought(allGuests,'apple pies',)))
+for i in fruits:
+    print("---{:<20}{:<4}".format(i,totalBrought(allGuests,i)))
