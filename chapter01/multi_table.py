71e94eb205d9c8cebe009a0d712ec1e4a0d652a0
nzj1981 <18322313385@163.com>
2017-11-02 10:38:49 +0800

    update multi_table.py 2time

--- chapter01/multi_table.py
+++ chapter01/multi_table.py
@@ -2,5 +2,5 @@
 
 for i in range(1,10):
     for j in range(1,i+1):
-        print("{} * {} = {:<2} ".format(j, i, i*j), end='')
+        print("{} * {} = {:<4}".format(j, i, i*j), end='')
     print()
