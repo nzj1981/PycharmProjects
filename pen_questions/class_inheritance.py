d199d1391bd649e4f8c14c1c9d86b605acfc8f2d
nzj1981 <18322313385@163.com>
2018-01-03 10:14:23 +0800

    add pen questions 2018.1.3

--- pen_questions/class_inheritance.py
+++ pen_questions/class_inheritance.py
@@ -14,8 +14,57 @@ obj = B()
 
 obj.show()
 
-# Q:如何调用类A的show方法
+# 1.Q:如何调用类A的show方法
 
 # A:__class__方法指向了类对象，只用给他赋值类型A，然后调用方法show。用完要修改回来
 obj.__class__ = A
 obj.show()
+
+def decToBOH(a, b):
+    s = set()
+    s = {'2', '8', '16'}
+    if str(b) not in s:
+        print('errors')
+        exit()
+    z = ''
+    if str(a).isdigit():
+        x = a
+        while True:
+            if x == 0:
+                break
+            y = x % b
+            x = x // b
+            z = str(y) + z
+    return z
+print(decToBOH(10, 2))
+
+def findMinAndMax(L):
+    mx, mn = (None, None)
+    if len(L) != 0:
+        t = tuple(L)
+        mx = max(t)
+        mn = min(t)
+    return (mn, mx)
+print(findMinAndMax([7,3]))
+
+def triangles():
+    n = 0
+    l = [1]
+    while 1:
+        yield l
+        l = [l[x] + l[x + 1] for x in range(n)]
+        l.insert(0, 1)
+        l.append(1)
+        n += 1
+    return l
+
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+a=[1]
+a[]
