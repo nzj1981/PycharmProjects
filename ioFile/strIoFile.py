1642e0711f13a22873f33e11d2301142904533dc
nzj1981 <18322313385@163.com>
2018-02-12 13:32:54 +0800

    update strIofile 2018.2.12

--- ioFile/strIoFile.py
+++ ioFile/strIoFile.py
@@ -14,17 +14,3 @@
 import os
 
 
-# !/usr/bin/env python3
-# encoding: utf-8
-# set( $SITE = "https://github.com/nzj1981/PycharmProjects.git" )
-
-""" 
-@version: v1.0 
-@author: autumner 
-@license: Apache Licence  
-@contact: 18322313385@163.com 
-@site: ${SITE} 
-@software: ${PRODUCT_NAME} 
-@file: ${NAME}.py 
-@time: ${DATE} ${TIME}
-"""
